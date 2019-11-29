42e7991515f4392af9c7ab8db4c4b6863098d6f4
chriseth <chris@ethereum.org>
2019-11-29 12:04:29 +0100

    Do not rematerialize in loops.

--- test/libsolidity/gasTests/abiv2_optimised.sol
+++ test/libsolidity/gasTests/abiv2_optimised.sol
@@ -17,9 +17,9 @@ contract C {
 // optimize-yul: true
 // ----
 // creation:
-//   codeDepositCost: 608400
-//   executionCost: 645
-//   totalCost: 609045
+//   codeDepositCost: 605600
+//   executionCost: 638
+//   totalCost: 606238
 // external:
 //   a(): 1029
 //   b(uint256): 2084

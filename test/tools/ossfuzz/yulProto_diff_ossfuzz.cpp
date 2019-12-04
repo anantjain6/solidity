e4b5b5cca80d6abcb457109aadfb5ec84ebf936c
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-12-04 13:13:35 +0100

    yul proto fuzzer: Target default evm version

--- test/tools/ossfuzz/yulProto_diff_ossfuzz.cpp
+++ test/tools/ossfuzz/yulProto_diff_ossfuzz.cpp
@@ -69,7 +69,7 @@ DEFINE_PROTO_FUZZER(Program const& _input)
 
 	// AssemblyStack entry point
 	AssemblyStack stack(
-		langutil::EVMVersion::istanbul(),
+		langutil::EVMVersion(),
 		AssemblyStack::Language::StrictAssembly,
 		dev::solidity::OptimiserSettings::full()
 	);
@@ -87,7 +87,7 @@ DEFINE_PROTO_FUZZER(Program const& _input)
 	yulFuzzerUtil::TerminationReason termReason = yulFuzzerUtil::interpret(
 		os1,
 		stack.parserResult()->code,
-		EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion::istanbul())
+		EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion())
 	);
 
 	if (termReason == yulFuzzerUtil::TerminationReason::StepLimitReached)
@@ -97,7 +97,7 @@ DEFINE_PROTO_FUZZER(Program const& _input)
 	termReason = yulFuzzerUtil::interpret(
 		os2,
 		stack.parserResult()->code,
-		EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion::istanbul()),
+		EVMDialect::strictAssemblyForEVMObjects(langutil::EVMVersion()),
 		(yul::test::yul_fuzzer::yulFuzzerUtil::maxSteps * 4)
 	);
 
